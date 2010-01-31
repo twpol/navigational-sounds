e883709fdeec18307a8e271fe3883ae81c6c5ca2
James Ross <silver@warwickcompsoc.co.uk>
2010-01-31 00:08:20 +0000

    Remove contents.rdf and add pref files to repo (these were included in 1.2.0).

---
+++ pref-defs.js
@@ -0,0 +1,21 @@
+// Start Navigation:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.navigation-start", true);
+
+// Complete Navigation:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.navigation-complete", true);
+
+// Blocked Pop-up Window:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.block-popup", true);
+
+// Information Bar:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.information-bar", true);
+
+// Feed Discovered:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.discover-feed", false);
+
+// Search Provider Discovered:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.discover-search-provider", true);
+
+// Complete Download:
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.download-complete", true);
+pref("extensions.{d84a846d-f7cb-4187-a408-b171020e8940}.event.download-complete.only-last", false);
---
+++ prefs.js
@@ -0,0 +1,26 @@
+function loadEvent() {
+	window.removeEventListener("load", setupTestButtons, false);
+	setupTestButtons();
+	updateTestButtons();
+	setInterval(updateTestButtons, 1000);
+}
+
+function setupTestButtons() {
+	var buttons = document.getElementsByClassName("sound-test");
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("command", testButtonCommand, false);
+	}
+}
+
+function testButtonCommand(e) {
+	navsounds.playSound(navsounds.getSystemSound(e.originalTarget.getAttribute("sound-app"), e.originalTarget.getAttribute("sound-name")));
+}
+
+function updateTestButtons() {
+	var buttons = document.getElementsByClassName("sound-test");
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].disabled = navsounds.getSystemSound(buttons[i].getAttribute("sound-app"), buttons[i].getAttribute("sound-name")) == "";
+	}
+}
+
+window.addEventListener("load", loadEvent, false);
